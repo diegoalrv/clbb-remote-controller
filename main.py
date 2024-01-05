bd29ee39f524d5142bbffadff17e251912518aca
fabian <fabianbustosby@gmail.com>
2024-01-05 11:11:14 -0300

    new struct and docker

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 import uvicorn
 
 # activar entorno: venv\Scripts\activate.bat
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="../frontend/templates")
-app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+templates = Jinja2Templates(directory="frontend/templates")
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
 # Inicio de pagina
 # @app.get("/")
@@ -23,6 +22,5 @@ async def read_item(request: Request):
         request=request, name="index.html"
     )
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=9090)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=9090)
