97e35be43a229e48894728747352502e003506c8
diegoalrv <d.alexis.ramirez@gmail.com>
2024-01-10 14:33:18 -0300

    Botones funcionando localhost

--- main.py
+++ main.py
@@ -22,5 +22,18 @@ async def read_item(request: Request):
         request=request, name="index.html"
     )
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=9090)
+# @app.get("/get_json") 
+# async def enviar_json():
+#     data = {
+#         "map_type": 2,
+#     }
+#     url_django = "http://127.0.0.1:8000/api/set_map_type/"
+#     response = requests.post(url_django, json=data)
+
+#     if response.status_code == 200:
+#         return{"mensaje": "XDDDDDDD"}
+#     else:
+#         return{"mensaje": ":("}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9090)
