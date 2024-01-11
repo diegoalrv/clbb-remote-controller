041d74568759432f51cbbc1210fe9d1b6a294bc6
fabian <fabianbustosby@gmail.com>
2024-01-11 17:11:47 -0300

    nuevos botones

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
-#import requests
+import requests
+import re
 
 # activar entorno: venv\Scripts\activate.bat
 
@@ -12,34 +13,34 @@ app = FastAPI()
 templates = Jinja2Templates(directory="frontend/templates")
 app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
-# Inicio de pagina
-# @app.get("/")
-# def index():
-#     return 'hello'
-
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html"
     )
-# @app.get("/obtenerdatos")
-# async def obtenerdatos():
-#     url_django = 
-#     response = requests.get(url_django)
-#     datos_django = response.json()
-#     return datos_django
-# @app.get("/get_json") 
-# async def enviar_json():
-#     data = {
-#         "map_type": 2,
-#     }
-#     url_django = "http://127.0.0.1:8000/api/set_map_type/"
-#     response = requests.post(url_django, json=data)
 
-#     if response.status_code == 200:
-#         return{"mensaje": "XDDDDDDD"}
-#     else:
-#         return{"mensaje": ":("}
+@app.get("/obtenerdatos")
+async def obtenerdatos():
+    url_django = 'http://192.168.31.120:8500/what_map/'
+    response = requests.get(url_django)
+    datos_django = response.json()
+    return datos_django
+
+#funcion para limpiar los datos
+def names_clean(json_data):
+    for item in json_data:
+        name = item["name"]
+        words = name.split('_')[0]
+        item["name"] = words.capitalize()
+    return json_data
+
+@app.get("/f", response_class=HTMLResponse)
+async def obtenerdatos(request: Request):
+    url_django = 'http://192.168.31.120:8500/what_map/'
+    response = requests.get(url_django) #obtiene los datos
+    datos_django = names_clean(response.json()) #limpia los datos
+    return templates.TemplateResponse("index2.html", {"request": request, "datos_django": datos_django})
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9090)
